2bc09e214a11f4bcc99b84c9b176887629e5f9d6
bonnycool <bonnydavid543@gmail.com>
2024-10-01 07:24:12 +0530

    initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 
 export const metadata = {
   title: 'My Blog Website',
-  description: 'A simple blog built with Next.js',
+  description: 'A simple blog built with Next.js a',
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
